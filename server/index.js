bd4cbf0c03a5888b53abea6c1d44043ce7cbe036
jiholee7 <114196883+jiholee7@users.noreply.github.com>
2023-08-07 23:34:13 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(express.json())
 app.use(cors(
   {
-    origin: ["https://course-helper.vercel.app"],
+    origin: ["https://course-helper-omega.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true
   }
