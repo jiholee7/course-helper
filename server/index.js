ba97cc2ad7de0b339beb6443b0b9dfbd8317130a
jiholee7 <114196883+jiholee7@users.noreply.github.com>
2023-08-08 01:40:26 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,8 @@ import { userRouter } from './src/routes/users.js'
 import 'dotenv/config.js'
 import { courseRouter } from './src/routes/courses.js'
 import { reviewRouter } from './src/routes/reviews.js'
-
+import { CourseModel } from '../models/Courses.js'
+import { verifyToken } from './users.js'
 
 const app = express()
 app.use(express.json())
@@ -13,8 +14,22 @@ app.use(express.json())
 app.use(cors())
 
 app.use('/auth', userRouter)
-app.use('/courses', courseRouter)
+//app.use('/courses', courseRouter)
 app.use('/reviews', reviewRouter)
+
+
+
+
+
+
+app.get('/courses/:name', async (req, res) => {
+  try{
+    const response = await CourseModel.find({name: req.params.name})
+    res.json(response)
+  } catch (error){
+    res.json(error)
+  }
+})
 
  app.get("/", (req, res) => {
    res.json("Hello")
