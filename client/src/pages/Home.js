9b30e1c485b6fa2865f76a782700cf41236c6da9
jiholee7 <114196883+jiholee7@users.noreply.github.com>
2023-08-08 01:34:18 -0400

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -106,7 +106,10 @@ const Home = ({ alertOpen, setAlertOpen, setAlertMessage }) => {
   const searchClass = async () => {
     const searchCourse = courseName.toUpperCase()
     try {
-      const res = await axios.get(`https://course-helper-api.vercel.app/courses/${searchCourse}`)
+      const res = await axios.get(`https://course-helper-api.vercel.app/`)
+      console.log(res)
+      //const res = await axios.get(`https://course-helper-api.vercel.app/courses/${searchCourse}`)
+
       if (res.data.length === 0){
         setCourseInfo(null)
         setReviews(null)
