0f28f6a04aa871ba3f69e6ed976484f2054256c7
jiholee7 <114196883+jiholee7@users.noreply.github.com>
2023-08-08 01:57:48 -0400

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -13,7 +13,7 @@ const Home = ({ alertOpen, setAlertOpen, setAlertMessage }) => {
   const [ cookies,  ] = useCookies(['access_token'])
   const [ errorMessage, setErrorMessage ] = useState(false)
   const [ reviewPage, setReviewPage ] = useState(1)
-  //axios.defaults.withCredentials = true
+  axios.defaults.withCredentials = true
 
 
   const [ reviewAverage, setReviewAverage ] = useState({
