c96ffa885d1c9e13d83c40ca0c2dc1eb799d6b14
jiholee7 <114196883+jiholee7@users.noreply.github.com>
2023-09-17 22:11:48 -0400

    Update Footer.js

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className='footer'>
       <img src='./coursehelper.png' alt='logo' />
-      <span>~Created By Jiho Lee~</span>
+      <span>~JL~</span>
       <span>Visit<a href='https://jiholee.vercel.app/' target='_blank'>My Website</a>for more info</span>
     </div>
   )
